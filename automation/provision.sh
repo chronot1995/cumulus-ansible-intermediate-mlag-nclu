198ebe519d4ad775c659ad31eab332c72f40d9d0
Mike Courtney <michael.courtney@linkedup.net>
2020-01-31 13:39:47 -0600

    1580499587

--- automation/provision.sh
+++ automation/provision.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ansible-playbook cumulus-ansible-intermediate-mlag-nclu.yml
+ansible-playbook -l switch01 cumulus-ansible-intermediate-mlag-nclu.yml
+sleep 10
+ansible-playbook -l switch02,server01 cumulus-ansible-intermediate-mlag-nclu.yml
